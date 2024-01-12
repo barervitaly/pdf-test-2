87623daf3218d87bd56103ad5eafc0360511b3d6
Vitaly <vitaly@vbwebarts.com>
2024-01-12 09:56:28 +0200

    debugging

--- pdf.js
+++ pdf.js
@@ -7,10 +7,9 @@ const pdf = async (req) => {
     });
     const page = await browser.newPage();
     await page.setContent(req.body.html); // Assuming HTML is sent in the request body
-    console.log("req.body.html");
+    console.log(req.body.html);
     // Take a screenshot
     await page.screenshot({ path: 'screenshot.png' });
-    await page.waitForTimeout(5000);
     const pdfBuffer = await page.pdf({ format: 'A4' });
     await browser.close();
     return pdfBuffer;
