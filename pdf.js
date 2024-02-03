20aba1eb0d8a813d7c81f5629045dbf9c3ae5ec1
Vitaly <vitaly@vbwebarts.com>
2024-02-03 18:02:10 +0200

    add timeout to pdf generation

--- pdf.js
+++ pdf.js
@@ -1,5 +1,26 @@
 const puppeteer = require('puppeteer');
 
+// const pdf = async (req) => {
+//     const browser = await puppeteer.launch({
+//         headless: true,
+//         args: ['--no-sandbox']
+//     });
+//     const page = await browser.newPage();
+//     await page.setContent(req.body.html); // Assuming HTML is sent in the request body
+//     // await page.goto(req.body.url, { waitUntil: 'networkidle0' });
+//     const pdfBuffer = await page.pdf({ format: 'A4', margin: {
+//         top: '20mm',
+//         bottom: '20mm'
+//         },
+//         printBackground: true,
+//         footerTemplate: '<span style="font-size: 10px; width: 100%; text-align: center;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></span>',
+//         displayHeaderFooter: true,
+//         preferCSSPageSize: false
+//     });
+//     await browser.close();
+//     return pdfBuffer;
+// }
+
 const pdf = async (req) => {
     const browser = await puppeteer.launch({
         headless: true,
@@ -8,9 +29,15 @@ const pdf = async (req) => {
     const page = await browser.newPage();
     await page.setContent(req.body.html); // Assuming HTML is sent in the request body
     // await page.goto(req.body.url, { waitUntil: 'networkidle0' });
-    const pdfBuffer = await page.pdf({ format: 'A4', margin: {
-        top: '20mm',
-        bottom: '20mm'
+
+    // Wait for 2 seconds to allow animations to finish
+    await page.waitForTimeout(2000);
+
+    const pdfBuffer = await page.pdf({
+        format: 'A4',
+        margin: {
+            top: '20mm',
+            bottom: '20mm'
         },
         printBackground: true,
         footerTemplate: '<span style="font-size: 10px; width: 100%; text-align: center;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></span>',
@@ -20,5 +47,6 @@ const pdf = async (req) => {
     await browser.close();
     return pdfBuffer;
 }
+
 
 module.exports = pdf;
